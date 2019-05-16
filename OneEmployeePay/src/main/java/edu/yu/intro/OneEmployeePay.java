b71b83af2e47357795a6102f0443a179bbfa6d18
avraham <avrahamleff@gmail.com>
2019-05-16 17:34:19 -0400

    Added OneEmployeeJava

---
+++ OneEmployeePay/src/main/java/edu/yu/intro/OneEmployeePay.java
@@ -0,0 +1,16 @@
+package edu.yu.intro;
+
+/** Add description of the assignment here.
+ * 
+ * @author yourname
+ */
+
+import java.util.Scanner;
+
+public class OneEmployeePay {
+
+  public static void main(String[] args) {
+    
+  }
+
+}
