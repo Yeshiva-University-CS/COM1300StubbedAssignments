fe6bfc24f0a126aa53cf74d671a405dc09c3dbb7
avraham <avrahamleff@gmail.com>
2019-05-16 17:26:50 -0400

    Added DozensOfEggsVariables

---
+++ DozensOfEggsVariables/src/main/java/edu/yu/intro/DozensOfEggsVariables.java
@@ -0,0 +1,12 @@
+package edu.yu.intro;
+
+/** Add a description of the assignment here.
+ *
+ * @author yourname
+ */
+
+public class DozensOfEggsVariables {
+  public static void main (final String[] args) {
+  }
+
+}
