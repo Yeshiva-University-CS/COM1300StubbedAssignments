d3cabecc9ec592eb6577c2ddd6ead91194c79710
avraham <avrahamleff@gmail.com>
2019-05-16 16:29:24 -0400

    Ensuring that pom.xml files don't get checked in.  Created HelloWorld assignment.

---
+++ HelloWorld/src/main/java/edu/yu/intro/HelloWorld.java
@@ -0,0 +1,12 @@
+package edu.yu.intro;
+
+/** "Hello World" for Yeshiva University.
+ *
+ * @author yourname
+ */
+
+public class HelloWorld {
+  public static void main (final String[] args) {
+  }
+  
+}
