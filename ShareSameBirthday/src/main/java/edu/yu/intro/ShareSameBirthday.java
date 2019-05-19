1f8a8379e22202fd5cd774a6088025ff34c2882e
avraham <avrahamleff@gmail.com>
2019-05-19 18:55:23 -0400

    Added ShareSameBirthday

---
+++ ShareSameBirthday/src/main/java/edu/yu/intro/ShareSameBirthday.java
@@ -0,0 +1,12 @@
+package edu.yu.intro;
+
+/** Program description here.
+ *
+ * @author yourname
+ */
+
+public class ShareSameBirthday {
+  public static void main (final String[] args) {
+  }   // main
+
+}
