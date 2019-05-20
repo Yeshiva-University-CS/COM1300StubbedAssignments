355876c1140e87a344b5e274f483b2c8c503fa16
avraham <avrahamleff@gmail.com>
2019-05-20 19:24:18 -0400

    Added RefactoredShareSameBirthday.

---
+++ RefactoredShareSameBirthday/src/main/java/edu/yu/intro/RefactoredShareSameBirthday.java
@@ -0,0 +1,20 @@
+package edu.yu.intro;
+
+/** Program description here.
+ *
+ * @author yourname
+ */
+
+public class RefactoredShareSameBirthday {
+    public static double runExperiments (final int populationSize,
+					 final int nExperiments) {
+	return 0.0;
+    }
+
+    public static boolean runSingleExperiment(final int populationSize) {
+	return false;
+    }
+
+    public static void main (final String[] args) {
+    }
+}
