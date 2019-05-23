f8b8de2e8822364896b4a362fb9562907144fea6
avraham <avrahamleff@gmail.com>
2019-05-23 11:26:23 -0400

    Added FindingPrimes

---
+++ FindingPrimes/src/main/java/edu/yu/intro/FindingPrimes.java
@@ -0,0 +1,38 @@
+package edu.yu.intro;
+
+/** Program description here
+ *
+ * @author yourname
+ */
+
+public class FindingPrimes {
+
+    public final static int MAX_LIMIT = 10000;
+
+    /** Return true iff N is prime.
+     *
+     * @throws IllegalArgumentException if N is less than 2 or greater than MAX_LIMIT
+     */
+    public static boolean isPrime(final int N) {
+	return false;
+    }  // isPrime
+
+    /** Returns an array of all integers in the range 1..N (inclusive) that are
+     * prime numbers
+     *
+     * @throws IllegalArgumentException if N is less than 2 or greater than MAX_LIMIT
+     */
+    public static int[] primes(final int N) {
+	return new int[0];
+    }
+
+    /** Print all primes up to args[0] (interpreted as an integer).
+     *
+     * @throws IllegalArgumentException if user-supplied input is not parseable
+     * as a positive integer.
+     * @throws IllegalArgumentException if limit is less than 2 or greater than MAX_LIMIT
+     */
+    public static void main (final String[] args) {
+    }
+
+}
